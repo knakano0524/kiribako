1cc8a815e18f99092197e0bdc76c57174309194f
Kenichi Nakano <knakano@nucl.phys.titech.ac.jp>
2018-10-31 14:25:02 +0900

    Added a playback mode to kireco.cc.

--- src/kireco.cc
+++ src/kireco.cc
@@ -2,7 +2,9 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
+#include <deque>
 #include <ctime>
 #include <sys/time.h>
 #include <webcam.h>
@@ -10,13 +12,29 @@ using namespace cv;
 using namespace std;
 
 void SetControl(CHandle handle, CControlId id, int value_new);
+void ChangePlaybackPos(int& idx_pb, int val);
+void SaveFrame(const int idx_pb);
+
+struct FrameData {
+  int fr;
+  Mat mat;
+  FrameData(const int fr_in, const Mat* mat_in) {
+    fr  = fr_in;
+    mat = mat_in->clone();
+  };
+};
+deque<FrameData> buffer_frame;
+
+typedef enum { DISPLAY, RECORD, PLAYBACK } KirecoMode_t;
+KirecoMode_t mode = DISPLAY;
 
 int main(int argc, char** argv)
 {
    int dev_num = 0;
    int fps = 15;
+   int n_buf = fps * 10;
    int opt;
-   while ((opt = getopt(argc, argv, "d:f:")) != -1) {
+   while ((opt = getopt(argc, argv, "d:f:b:")) != -1) {
       switch (opt) {
       case 'd':
          dev_num = atoi(optarg);
@@ -25,6 +43,10 @@ int main(int argc, char** argv)
       case 'f':
          fps = atoi(optarg);
          cout << "FPS (-f) = " << fps << ".\n";
+         break;
+      case 'b':
+         n_buf = atoi(optarg);
+         cout << "N of buffered frames (-b) = " << n_buf << ".\n";
          break;
       default:
          cerr << "Invalid option.  Abort.\n";
@@ -79,105 +101,135 @@ int main(int argc, char** argv)
    namedWindow("frame");
 
    unsigned int i_fr = 0;
+   int idx_pb = 0;
    struct timeval time0, time1;
    Mat frame_pre;
-   bool do_record = false;
+   //bool do_record = false;
    bool take_diff = false;
    bool do_negate = false;
    bool loop      = true;
    while (loop) {
-      gettimeofday(&time0, 0);
-      Mat frame_now;
-      cap >> frame_now;
-      //cvtColor(frame, edges, CV_BGR2GRAY);
-      //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-      //Canny(edges, edges, 0, 30, 3);
+     gettimeofday(&time0, 0);
+
+     if (mode == PLAYBACK) {
+       FrameData* fd = &buffer_frame.at(idx_pb);
+       imshow("frame", fd->fr);
+     } else { // DISPLAY or RECORD
+       Mat frame_now;
+       cap >> frame_now;
+       i_fr++;
       
-      Mat frame;
-      if (! take_diff) {
+       Mat frame;
+       if (! take_diff) {
          frame = frame_now;
-      } else {
-         if (frame_pre.rows == 0) {
-            frame_pre = frame_now;
-            continue;
+       } else {
+         if (frame_pre.rows == 0) { // frame_pre is not set
+           frame_pre = frame_now;
+           continue;
          }
          absdiff(frame_pre, frame_now, frame);
          frame_pre = frame_now;
-      }
-      if (do_negate) {
+       }
+       if (do_negate) {
          static Mat mat0 = Mat(frame.rows, frame.cols, frame.type(), Scalar(1,1,1))*255;
          subtract(mat0, frame, frame);
-      }
+       }
       
-      time_t utime = time(0);
-      tm* ltime = localtime(&utime);
-      char stime[256];
-      strftime(stime, sizeof(stime),"%Y-%m-%d %H:%M:%S", ltime);
-      char msg[256];
-      sprintf(msg, "%s %06i", stime, i_fr);
-      cv::putText (frame, msg, cv::Point (50,50), cv::FONT_HERSHEY_SIMPLEX, 2,
-                   cv::Scalar (100, 50, 50), 5, CV_AA);
-      if (do_record) {
-         if (i_fr == 0) writer.open(fn_out, CV_FOURCC('M','J','P','G'), fps, cap_size);
+       time_t utime = time(0);
+       tm* ltime = localtime(&utime);
+       char stime[256];
+       strftime(stime, sizeof(stime),"%Y-%m-%d %H:%M:%S", ltime);
+       char msg[256];
+       sprintf(msg, "%s %06i", stime, i_fr);
+       cv::putText (frame, msg, cv::Point (50,50), cv::FONT_HERSHEY_SIMPLEX, 2,
+                    cv::Scalar (100, 50, 50), 5, CV_AA);
+       if (mode == RECORD) {
+         if (! writer.isOpened()) {
+           if (! writer.open(fn_out, CV_FOURCC('M','J','P','G'), fps, cap_size)) {
+             cerr << "!!ERROR!!  Failed at opening the output file.  Abort.\n";
+             exit(1);
+           }
+         }
          writer << frame;
-         i_fr++;
          cv::putText(frame, "REC", cv::Point(cap_size.width - 130, 50),
                      cv::FONT_HERSHEY_SIMPLEX, 2,
                      cv::Scalar(0, 0, 255), 5, CV_AA);
-      }
-      imshow("frame", frame);
-      
-      gettimeofday(&time1, NULL);
-      int time_w = (int)round(1000.0/fps - 1e3*(time1.tv_sec - time0.tv_sec) - 1e-3*(time1.tv_usec - time0.tv_usec));
-      if (time_w <= 0) {
-         cout << "Wait time = " << time_w << " us.  FPS is too large." << endl;
-         time_w = 1;
-      }
-      switch(waitKey(time_w)){
-         //Logicool C615 focus has 17 values, 0, 17, 34, 51, , , 238, 255
-      case 'p':
-         c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
-         if (value.value < 255) value.value += 17;
-         c_set_control(handle, CC_FOCUS_ABSOLUTE, &value);
-         ret = c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
-         cout << "Focus = " << value.value << "\n";
-         break;
-      case 'm':
-         c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
-         if (value.value > 0) value.value -= 17;
-         c_set_control(handle, CC_FOCUS_ABSOLUTE, &value);
-         ret = c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
-         cout << "Focus = " << value.value << "\n";
-         break;
-      case 'r':
-         cout << "Start recording." << endl;
-         do_record = true;
-         break;
-      case 's':
-         cout << "Stop recording." << endl;
-         do_record = false;
-         break;
-      case 'd':
-         cout << "Set the frame-difference mode to '" << !take_diff << "'." << endl;
-         take_diff = ! take_diff;
-         break;
-      case 'n':
-         cout << "Set the negative mode to '" << !do_negate << "'." << endl;
-         do_negate = ! do_negate;
-         break;
-      case 'c':
-         cout << "Control." << endl;
-         oss.str("");
-         oss << "guvcview --device=/dev/video" << dev_num << " --control_panel &";
-         system(oss.str().c_str()); 
-         break;
-      case 'q':
-         cout << "Quit." << endl;
-         loop = false;
-         break;
-      default:
-         break;
-      }
+       }
+       imshow("frame", frame);
+     
+       buffer_frame.push_back(FrameData(i_fr, &frame));
+       if (buffer_frame.size() > (unsigned)n_buf) buffer_frame.pop_front();
+     }
+     
+     gettimeofday(&time1, NULL);
+     int time_w = (int)round(1000.0/fps - 1e3*(time1.tv_sec - time0.tv_sec) - 1e-3*(time1.tv_usec - time0.tv_usec));
+     if (time_w <= 0) {
+       cout << "Wait time = " << time_w << " us.  FPS is too large." << endl;
+       time_w = 1;
+     }
+     switch(waitKey(time_w)){
+       //Logicool C615 focus has 17 values, 0, 17, 34, 51, , , 238, 255
+     case 'p':
+       c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
+       if (value.value < 255) value.value += 17;
+       c_set_control(handle, CC_FOCUS_ABSOLUTE, &value);
+       ret = c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
+       cout << "Focus = " << value.value << "\n";
+       break;
+     case 'm':
+       c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
+       if (value.value > 0) value.value -= 17;
+       c_set_control(handle, CC_FOCUS_ABSOLUTE, &value);
+       ret = c_get_control(handle, CC_FOCUS_ABSOLUTE, &value);
+       cout << "Focus = " << value.value << "\n";
+       break;
+     case 'r':
+       cout << "Start recording." << endl;
+       mode = RECORD;
+       break;
+     case 's':
+       cout << "Stop recording." << endl;
+       mode = DISPLAY;
+       break;
+     case 'd':
+       cout << "Set the frame-difference mode to '" << !take_diff << "'." << endl;
+       take_diff = ! take_diff;
+       break;
+     case 'n':
+       cout << "Set the negative mode to '" << !do_negate << "'." << endl;
+       do_negate = ! do_negate;
+       break;
+     case 'c':
+       cout << "Control." << endl;
+       oss.str("");
+       oss << "guvcview --device=/dev/video" << dev_num << " --control_panel &";
+       system(oss.str().c_str()); 
+       break;
+     case '.':
+       ChangePlaybackPos(idx_pb, +1);
+       break;
+     case ',':
+       ChangePlaybackPos(idx_pb, -1);
+       break;
+     case '>':
+       ChangePlaybackPos(idx_pb, +fps);
+       break;
+     case '<':
+       ChangePlaybackPos(idx_pb, -fps);
+       break;
+     case '/':
+       mode = DISPLAY;
+       break;
+     case '?':
+       SaveFrame(idx_pb);
+       break;
+     case 'q':
+       cout << "Quit." << endl;
+       loop = false;
+       break;
+     default:
+       break;
+     }
    }
    
    c_close_device(handle);
@@ -202,3 +254,26 @@ void SetControl(CHandle handle, CControlId id, int value_new)
   }
 }
 
+void ChangePlaybackPos(int& idx_pb, int val)
+{
+  int n_fr = buffer_frame.size();
+  if (mode != PLAYBACK) {
+    mode = PLAYBACK;
+    idx_pb = n_fr;
+  }
+  int idx_new = idx_pb + val;
+  if (idx_new < 0     ) idx_new = 0;
+  if (idx_new > n_fr-1) idx_new = n_fr-1;
+  idx_pb = idx_new;
+}
+
+void SaveFrame(const int idx_pb)
+{
+  if (mode != PLAYBACK) return;
+  FrameData* fd = &buffer_frame.at(idx_pb);
+  ostringstream oss;
+  oss << "kireco_" << setfill('0') << setw(6) << fd->fr << ".png";
+  string fn_out = oss.str();
+  cout << "  Save " << fn_out << endl;
+  imwrite(fn_out, fd->mat);
+}
